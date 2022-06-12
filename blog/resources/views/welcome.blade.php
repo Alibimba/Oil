cf1adb1b6765cacaf9c01467cdd5a07abac94f52
Alibimba <lex2903@mail.ru>
2022-06-12 15:21:36 +0500

    Начало

--- blog/resources/views/welcome.blade.php
+++ blog/resources/views/welcome.blade.php
@@ -1,13 +1,27 @@
 <!doctype html>
-<html lang="en">
+<html lang="ru">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./css/glav.css">
     <title>Document</title>
 </head>
 <body>
+<div class="menu">
+    <div class="menu_block">
+        <div class="menu_logo">
+            <img id="logo-img" src="./image/logo.png">
+        </div>
+        <div class="menu_right-side">
+            <div class="menu_li"> Каталог</div>
 
+            <div class="menu_li"> Контакты</div>
+            <div class="menu_li">О компании</div>
+        </div>
+    </div>
+
+</div>
 </body>
 </html>
