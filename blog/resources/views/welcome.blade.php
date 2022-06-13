6439a473e9572a7d286e84902e12724799d7ee61
Alibimba <lex2903@mail.ru>
2022-06-13 15:51:42 +0500

    Каталог внешний. БЕЗ JS кода
    Но с footer
    Yeah  :)

--- blog/resources/views/welcome.blade.php
+++ blog/resources/views/welcome.blade.php
@@ -22,6 +22,59 @@
         </div>
     </div>
 
+
 </div>
+
+<div class="body">
+    <div class="content">
+        <div class="cart">
+            <div class="cart-img"></div>
+            <div class="info">
+                <div class="cart-text">Абоба</div>
+                <div class="cart-price">Бобаба</div>
+                <div class="btn">КУПИ</div>
+            </div>
+        </div>
+        <div class="cart">
+            <div class="cart-img"></div>
+            <div class="cart-text"></div>
+            <div class="cart-price"></div>
+            <div class="btn"></div>
+        </div>
+        <div class="cart">
+            <div class="cart-img"></div>
+            <div class="cart-text"></div>
+            <div class="cart-price"></div>
+            <div class="btn"></div>
+        </div>
+        <div class="cart">
+            <div class="cart-img"></div>
+            <div class="cart-text"></div>
+            <div class="cart-price"></div>
+            <div class="btn"></div>
+        </div>
+        <div class="cart">
+            <div class="cart-img"></div>
+            <div class="cart-text"></div>
+            <div class="cart-price"></div>
+            <div class="btn"></div>
+        </div>
+    </div>
+</div>
+<footer>
+    <div class="logo-info-foot">
+        <div class="logo-foot"><img id="logo-img" src="./image/logo.png"></div>
+
+        <hr id="hr">
+        <div class="info-foot">Крупнейший<br> дистрибьютор автомасел и<br> спецжидкостей</div>
+    </div>
+    <div class="contact">
+        <div class="contact-email">Email: <a href="#">Petya23@mail.ru</a></div>
+        <div class="contact-email">Телефон: <a href="#">89637542734</a></div>
+        <div class="contact-email">г. Курган, ул. Омская, 145, корп.1</div>
+    </div>
+    <div class="time"> 16:00 - 16:30</div>
+
+</footer>
 </body>
 </html>
