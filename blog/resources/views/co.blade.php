d3f19989c7010b8d2a984894677df5063620cb04
Alibimba <lex2903@mail.ru>
2022-06-14 16:46:40 +0500

    КУШБЕЛЬДОН КУШБЕЛЬДОН КУШБЕЛЬДОН КУШБЕЛЬДОН КУШБЕЛЬДОН КУШБЕЛЬДОН КУШБЕЛЬДОН КУШБЕЛЬДОН

---
+++ blog/resources/views/co.blade.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contacnte</title>
+    <link rel="stylesheet" href="./css/glav.css">
+</head>
+<body>
+
+<div class="menu">
+    <div class="menu_block">
+        <div class="menu_logo">
+            <img id="logo-img" src="./image/logo.png">
+        </div>
+        <div class="menu_right-side">
+            <div class="menu_li"> Каталог</div>
+
+            <div class="menu_li"> Контакты</div>
+            <div class="menu_li">О компании</div>
+        </div>
+    </div>
+
+
+</div>
+
+
+
+
+<div class="obsh">
+    <div class="chel">
+        <div class="chel_img"><img id="1" src="./image/img_6.png" height="300" width="200"> </div>
+        <div class="chel_name">ЧОНГУК</div>
+        <div class="chel_dol">ВОКАЛИСт</div>
+        <div class="chel_nomber">В ГРУППЕ ВК!!!!</div>
+    </div>
+    <div class="chel">
+        <div class="chel_img"><img  id="1" src="./image/img_7.png" height="300" width="200"></div>
+        <div class="chel_name">Ченгунчек</div>
+        <div class="chel_dol">Моет туалеты</div>
+        <div class="chel_nomber">894828184</div>
+    </div>
+    <div class="chel">
+        <div class="chel_img"><img id="1" src="./image/img_8.png" height="300" width="200"></div>
+        <div class="chel_name">БАРБЕНДЕК</div>
+        <div class="chel_dol">Моет окна</div>
+        <div class="chel_nomber">8849491824</div>
+    </div>
+    <div class="chel">
+        <div class="chel_img"><img id="1" src="./image/img_9.png" height="300" width="200"></div>
+        <div class="chel_name">БАРАН</div>
+        <div class="chel_dol">Моет тех, кто моет окна и туалеты</div>
+        <div class="chel_nomber">Уже в твоих контактах)</div>
+    </div>
+    <div class="chel">
+        <div class="chel_img"><img id="1" src="./image/img_10.png" height="300" width="200"></div>
+        <div class="chel_name">КУШБЕЛЬДОН</div>
+        <div class="chel_dol">КУШБЕЛЬДОН</div>
+        <div class="chel_nomber">КУШБЕЛЬДОН</div>
+    </div>
+</div>
+
+
+
+
+<footer>
+    <div class="logo-info-foot">
+        <div class="logo-foot"><img id="logo-img" src="./image/logo.png"></div>
+        <hr id="hr">
+        <div class="info-foot">Крупнейший<br> дистрибьютор автомасел и<br> спецжидкостей</div>
+    </div>
+    <div class="contact">
+        <div class="contact-email">Email: <a href="#">Petya23@mail.ru</a></div>
+        <div class="contact-email">Телефон: <a href="#">89637542734</a></div>
+        <div class="contact-email">г. Курган, ул. Омская, 145, корп.1</div>
+    </div>
+    <div class="time"> 16:00 - 16:30</div>
+
+</footer>
+
+</body>
+</html>
--- blog/resources/views/welcome.blade.php
+++ blog/resources/views/welcome.blade.php
@@ -17,7 +17,7 @@
         <div class="menu_right-side">
             <div class="menu_li"> Каталог</div>
 
-            <div class="menu_li"> Контакты</div>
+            <div class="menu_li " onclick="location.href='/co';"> Контакты</div>
             <div class="menu_li">О компании</div>
         </div>
     </div>
